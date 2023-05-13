b4ecbcd145a505528612e219e820f7191950c5b9
twishapatel12 <133378225+twishapatel12@users.noreply.github.com>
2023-05-13 12:15:51 +0530

    qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -1,3 +1,3 @@
 import qrcode as qr
-img = qr.make("http://localhost/wordpress/")
-img.save("travel_shuffle.png")
+img = qr.make("https://www.google.com/maps/place/Tirth+Construction/@22.3185057,73.2124321,18z/data=!4m6!3m5!1s0x395fcf46a30d738d:0x6c2df2ff5359df71!8m2!3d22.3184608!4d73.2126599!16s%2Fg%2F11kjpry9mp")
+img.save("tirth_construction.png")
